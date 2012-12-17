e3bd2ef762e833c941861f5e10529c6f3faca95f
Juuso Stromberg <juuso.stromberg@cs.helsinki.fi>
2012-12-17 11:07:08 +0200

    Gladiaattori- ja Joukkue-luokkien luonti

--- Gladiaattorit/src/gladiaattorit/Main.java
+++ Gladiaattorit/src/gladiaattorit/Main.java
@@ -1,11 +1,30 @@
-
 package gladiaattorit;
 
+import gladiaattorit.logiikka.Gladiaattori;
+import gladiaattorit.logiikka.Joukkue;
+import java.util.Scanner;
 
+/**
+ *
+ * @author juzmach
+ */
 public class Main {
 
-    
     public static void main(String[] args) {
-       
+        Scanner lukija = new Scanner(System.in);
+        System.out.println("Anna joukkueellesi nimi:");
+        String joukkueenNimi = lukija.nextLine();
+        Joukkue joukkue = new Joukkue(joukkueenNimi);
+        
+        System.out.println("Monta gladiaattoria haluat?");
+        int maara = Integer.parseInt(lukija.nextLine());
+        
+        for (int i = 0; i < maara; i++) {
+            System.out.println("Anna gladiaattorin nimi:");
+            String gladiaattorinNimi = lukija.nextLine();
+            joukkue.lisaaGladiaattori(new Gladiaattori(gladiaattorinNimi));
+        }
+        
+        System.out.println(joukkue.toString());
     }
 }
